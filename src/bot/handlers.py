fca7d2290ad94810c327efa92c699c32f9aff525
Anton Braun <merkme@yandex.ru>
2024-04-29 12:05:09 +0300

    Fixed problem with Django and telegram-api-bot

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -6,17 +6,11 @@ from telegram.ext import (
     MessageHandler
 )
 
-def log_errors(f):
-    def inner(*args, **kwargs):
-        try:
-            return f(*args, **kwargs)
-        except Exception as e:
-            error_message = f'Произошла ошибка: {e}'
-            print(error_message)
-            raise e
-    return inner
+from core.logging import log_errors
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handler for the /start command."""
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=(
@@ -24,8 +18,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             'Здесь ты найдёшь лучших преподавателей.')
     )
 
+
 @log_errors
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handler for echoing user messages."""
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=update.message.text
