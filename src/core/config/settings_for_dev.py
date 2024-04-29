5cdf3fec0846c0df329885f444e9c9a2479b46ed
unknown <evgeny-fastunov@yandex.ru>
2024-04-29 10:55:23 +0300

    Добавить разделение settings Джанго на dev, prod, tests

---
+++ src/core/config/settings_for_dev.py
@@ -0,0 +1,11 @@
+"""Settings for development."""
+from core.config.settings_base import *  # noqa
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
---
+++ src/core/config/settings_for_prod.py
@@ -0,0 +1,22 @@
+"""Settings for production."""
+from core.config.settings_base import *  # noqa
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
