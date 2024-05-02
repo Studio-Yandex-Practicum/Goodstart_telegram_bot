3eeebefbc3e8903bb651a310160d8a93eee153bc
unknown <evgeny-fastunov@yandex.ru>
2024-05-02 09:23:12 +0300

    add fix for settings

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -2,6 +2,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.config.settings_dev")  # настройки для разработки
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.config.settings_tests")  # настройки для тестирования
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.config.settings_prod")  # настройки для сервера
 
 application = get_asgi_application()
--- src/core/config/settings_for_prod.py
+++ src/core/config/settings_for_prod.py
@@ -1,14 +1,7 @@
 """Settings for production."""
 from core.config.settings_base import *  # noqa
 
-DEBUG = True
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DEBUG = False
 
 DATABASES = {
     "default": {
