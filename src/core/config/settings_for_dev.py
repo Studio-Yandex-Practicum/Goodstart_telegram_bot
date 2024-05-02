039f5de00aea17b40fb97ee4f8464d4219272206
unknown <evgeny-fastunov@yandex.ru>
2024-05-02 10:25:17 +0300

    add fix for settings

--- src/core/config/settings_for_dev.py
+++ src/core/config/settings_for_dev.py
@@ -5,7 +5,11 @@ DEBUG = True
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
