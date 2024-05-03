733d5152a1a2d0589c9ef30835c089b199dc8b17
Евгений Фастунов <130343633+evgeny-fastunov@users.noreply.github.com>
2024-05-03 10:51:38 +0300

    Добавить разделение settings Джанго на dev, prod, tests (#14)
    Co-authored-by: Konstantin Raikhert <69113745+KonstantinRaikhert@users.noreply.github.com>

---
+++ src/core/asgi_dev.py
@@ -0,0 +1,7 @@
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.config.settings_dev")  # настройки для разработки
+
+application = get_asgi_application()
---
+++ src/core/config/settings_for_tests.py
@@ -0,0 +1,11 @@
+"""Settings for running tests."""
+from core.config.settings_base import *  # noqa
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
