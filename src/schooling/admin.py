275433f8ebd8ee700d49903d1147d80a81421fb4
Konstantin Raikhert <raikhert13@gmail.com>
2024-05-13 12:07:49 +0300

    update models in schooling

--- src/schooling/admin.py
+++ src/schooling/admin.py
@@ -7,7 +7,11 @@ from schooling.models import Student, Teacher, Subject
 class TeacherAdmin(admin.ModelAdmin):
     """Управление преподавателями."""
 
-    list_display = ('name', 'surname', 'competence')
+    list_display = ('name', 'surname', 'get_competences')
+
+    def get_competences(self, obj):
+        """Return competences."""
+        return '\n'.join([c.competence for c in obj.competence.all()])
 
 
 @admin.register(Student)
@@ -21,5 +25,5 @@ class StudentAdmin(admin.ModelAdmin):
 class SubjectAdmin(admin.ModelAdmin):
     """Управление школьными предметами."""
 
-    list_display = ("name",)
-    exclude = ("subject_key",)
+    list_display = ('name',)
+    exclude = ('subject_key',)
