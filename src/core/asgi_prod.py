2cb1b03edd8a71ef5a922c180eb877fccc4f068f
Diana Kim <Dianakim.1993@yandex.ru>
2024-05-19 01:41:39 +0900

    Fix bot shutdown and adds ASGI Lifespan Protocol

--- src/core/asgi_prod.py
+++ src/core/asgi_prod.py
@@ -2,9 +2,26 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from bot.bot_interface import Bot
+
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
     'core.config.settings_for_prod',
 )  # настройки для сервера
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+
+async def application(scope, receive, send):
+    """ASGI application to handle incoming ASGI events."""
+    if scope['type'] == 'lifespan':
+        while True:
+            message = await receive()
+            if message['type'] == 'lifespan.startup':
+                await send({'type': 'lifespan.startup.complete'})
+            elif message['type'] == 'lifespan.shutdown':
+                Bot().stop()
+                await send({'type': 'lifespan.shutdown.complete'})
+                break
+    else:
+        await django_asgi_app(scope, receive, send)
