24fd24022255731e1a278886b0eba010742ad8bc
Сергей Жуланов <seregaskayoker@yandex.ru>
2024-05-28 02:21:36 +0500

    Тестовые данные студентов

---
+++ src/schooling/management/commands/create_test_students.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from schooling.factories import create_students
+
+
+class Command(BaseCommand):
+    """Command to Student instances creation for the project tests."""
+
+    def _generate(self):
+        create_students()
+
+    def handle(self, *args, **options):
+        """Call the method `_generate` for the test Student creation."""
+        self._generate()
