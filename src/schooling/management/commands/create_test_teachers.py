39211f1b4a825a8b204826a82e3ed77109921c36
shkiperzero <ibazhov92@gmail.com>
2024-05-29 00:27:11 +0500

    docstring corrected

--- src/schooling/management/commands/create_test_teachers.py
+++ src/schooling/management/commands/create_test_teachers.py
@@ -4,11 +4,11 @@ from schooling.factories import create_teachers
 
 
 class Command(BaseCommand):
-    """Command to Student instances creation for the project tests."""
+    """Command to Teacher instances creation for the project tests."""
 
     def _generate(self):
         create_teachers()
 
     def handle(self, *args, **options):
-        """Call the method `_generate` for the test Student creation."""
+        """Call the method `_generate` for the test Teacher creation."""
         self._generate()
