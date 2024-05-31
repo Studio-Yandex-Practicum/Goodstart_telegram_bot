9263d9135e0210e9c27e85d9879ec076aacf713d
Александр Литвинов <alexpunder@yandex.ru>
2024-05-31 19:01:41 +0300

    Добавлен список __all__ импортируемых модулей в src/bot/handlers/__init__.py

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,3 +1,5 @@
 from bot.handlers.echo import echo_handler  # noqa
 from bot.handlers.start import start_handler  # noqa
 from bot.handlers.help import help_handler  # noqa
+
+__all__ = ['echo_handler', 'start_handler', 'help_handler',]
