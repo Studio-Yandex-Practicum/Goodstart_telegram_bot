c24dfbce503a5830ff0fdd616c769e6bde5532b3
Александр Литвинов <alexpunder@yandex.ru>
2024-05-31 19:05:13 +0300

    Убраны #noqa.

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,5 +1,5 @@
-from bot.handlers.echo import echo_handler  # noqa
-from bot.handlers.start import start_handler  # noqa
-from bot.handlers.help import help_handler  # noqa
+from bot.handlers.echo import echo_handler
+from bot.handlers.start import start_handler
+from bot.handlers.help import help_handler
 
 __all__ = ['echo_handler', 'start_handler', 'help_handler',]
