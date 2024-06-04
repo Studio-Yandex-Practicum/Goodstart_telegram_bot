47a4f9dd46fee4255d45a065c1f810456bb90f68
Sergey Mazilin <sergeypaly4@gmail.com>
2024-06-04 13:04:57 +0400

    Update settings_base.py
    правки кодстайла ruff

--- src/core/config/settings_base.py
+++ src/core/config/settings_base.py
@@ -23,7 +23,6 @@ ALLOWED_HOSTS = ['*']
 DEFAULT_APPS = [
     'material',
     'material.admin',
-    # 'django.contrib.admin',
     'core.apps.GoodStartAdminConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,23 +132,9 @@ BASE_URL = os.getenv('BASE_URL', 'http://127.0.0.1:8000')
 MATERIAL_ADMIN_SITE = {
     'HEADER':  _('Административная панель'),  # Admin site header
     'TITLE':  _('Административная панель'),  # Admin site title
-    # 'FAVICON':  'base/icons8-favicon-16.png',  # Admin site favicon (path to static should be specified)
-    # 'MAIN_BG_COLOR':  'color',  # Admin site main color, css color should be specified
-    # 'MAIN_HOVER_COLOR':  'color',  # Admin site main hover color, css color should be specified
-    # 'PROFILE_PICTURE':  'font.jpg',  # Admin site profile picture (path to static should be specified)
-    # 'PROFILE_BG':  'font.jpg',  # Admin site profile background (path to static should be specified)
-    # 'LOGIN_LOGO':  'font.jpg',  # Admin site logo on login page (path to static should be specified)
-    # 'LOGOUT_BG':  'font.jpg',  # Admin site background on login/logout pages (path to static should be specified)
     'SHOW_THEMES':  True,  # Show default admin themes button
-    # 'TRAY_REVERSE': True,  # Hide object-tools and additional-submit-line by default
     'NAVBAR_REVERSE': True,  # Hide side navbar by default
     'SHOW_COUNTS': True,  # Show instances counts for each model
-    # 'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
-    #     'sites': 'send',
-    # },
-    # 'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
-    #     'site': 'contact_mail',
-    # }
 }
 PERSISTENCE_DIR = ROOT_DIR / 'persistence_data'
 PERSISTENCE_PATH = PERSISTENCE_DIR / 'persistence_file'
