689123226f9f3868c4843c3c5dfc5f6db179f380
Sergey Mazilin <mazilin.sergey@yandex.ru>
2024-06-06 13:09:15 +0400

    убрал дублирование approve_applications после merge

--- src/potential_user/admin.py
+++ src/potential_user/admin.py
@@ -35,10 +35,3 @@ class ApplicationFormAdmin(admin.ModelAdmin):
         for query in queryset:
             query.approved = True
             query.save()
-
-    @admin.action(description='Подтвердить выбранные заявки')
-    def approve_applications(self, request, queryset):
-        """Перевести все выбранные заявки в статус 'Подтверждено'."""
-        for query in queryset:
-            query.approved = True
-            query.save()
