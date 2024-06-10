59e6481b8d14457b4c8d6066124acb0f13dbd0d1
Сергей Жуланов <seregaskayoker@yandex.ru>
2024-06-11 02:22:29 +0500

    Функционал работает, но требуется рефакторинг

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -3,4 +3,7 @@ from bot.handlers.start import start_handler
 from bot.handlers.help import help_handler
 from bot.handlers.schedule import schedule_handler
 
-__all__ = ['echo_handler', 'start_handler', 'help_handler', 'schedule_handler',]
+__all__ = [
+    'echo_handler', 'start_handler', 'help_handler',
+    'success_registration_webapp_handler', 'schedule_handler',
+]
