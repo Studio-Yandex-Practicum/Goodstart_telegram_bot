00f6fd3c53a24091d94cc6c928a59611d32f32c1
Sergey Mazilin <mazilin.sergey@yandex.ru>
2024-06-11 13:09:01 +0400

    набросал инфру для деплоя

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python manage.py makemigrations
+python manage.py collectstatic --noinput
+export PYTHONPATH=${PWD}
+python manage.py migrate --check
+
+status=$?
+if [[ $status != 0 ]]; then
+  python manage.py migrate
+fi
+exec "$@"
