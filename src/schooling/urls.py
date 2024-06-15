0fc5458fb95954d7bed60fa1b0ae87a6b8947ceb
alexpunder <131157269+alexpunder@users.noreply.github.com>
2024-06-15 13:38:26 +0300

    Feature/detail schedule endpoint (#135)
    Co-authored-by: Konstantin Raikhert <69113745+KonstantinRaikhert@users.noreply.github.com>

--- src/schooling/urls.py
+++ src/schooling/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-from schooling.views import schedule_page
+from schooling.views import (
+    schedule_page, details_schedule_page, cancel_lesson,
+    change_datetime_lesson,
+)
 
 app_name = 'schedule'
 
 urlpatterns = [
     path('<int:id>/', schedule_page, name='schedule'),
+    path(
+        '<int:id>/<int:lesson_id>/',
+        details_schedule_page,
+        name='details_schedule',
+    ),
+    path(
+        '<int:id>/<int:lesson_id>/change-datetime-lesson/',
+        change_datetime_lesson,
+        name='change_datetime_lesson',
+    ),
+    path(
+        '<int:id>/<int:lesson_id>/cancel-lesson/',
+        cancel_lesson,
+        name='cancel_lesson',
+    ),
 ]
