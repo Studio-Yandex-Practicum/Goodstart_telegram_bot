dfc1df6b11f2c24766b4eeea1c5f554d9c09d730
Neizalik <golovastykxx@gmail.com>
2024-06-17 15:39:47 +0300

    Расписал хэндлер для кнопки "Что умеет бот"

--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.urls import reverse
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 
+from bot.states import UserStates
+
 
 async def get_root_markup(telegram_id):
     keyboard = [
             [
                 InlineKeyboardButton('Что умеет бот',
-                                     callback_data='help'),
+                                     callback_data=UserStates.HELP.value),
                 InlineKeyboardButton(
                     text='Посмотреть расписание',
                     web_app=WebAppInfo(
