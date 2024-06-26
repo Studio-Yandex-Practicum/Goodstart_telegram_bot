668e9ebd8d98b0ad83c188910383c70d41dbb31f
Ruslan Kuznetsov <91840758+Noc0mments@users.noreply.github.com>
2024-06-26 22:43:44 +0300

    add new message to echo#163 (#168)

--- src/bot/handlers/echo.py
+++ src/bot/handlers/echo.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes, MessageHandler, filters
 
 from core.logging import log_errors
+from bot.handlers.help import help
 
 
 @log_errors
@@ -9,7 +10,8 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Echo messages handler."""
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=update.message.text,
+        text='Неизвестная команда, выберите что-то из того, что я умею!',
     )
+    await help(update, context)
 
 echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
