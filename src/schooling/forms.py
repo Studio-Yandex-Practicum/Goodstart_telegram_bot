8b89271951fae2576af109debcd6c1aaa23dd166
Konstantin Raikhert <raikhert13@gmail.com>
2024-07-16 19:23:21 +0300

    refactoring schooling form

--- src/schooling/forms.py
+++ src/schooling/forms.py
@@ -1,29 +1,11 @@
-from datetime import date, timedelta
-
 from django import forms
-from django.utils.translation import gettext_lazy as _
 
 from schooling.models import Lesson
-
-
-def validation_intersections_time_periods(
-    list_periods,
-    start_1,
-    time_1,
-):
-    """Проверка входит новый период занятия к остаольным."""
-    for scheduled_lesson in list_periods:
-        start_2 = scheduled_lesson.datetime_start
-        time_2 = scheduled_lesson.duration
-        end_1 = (start_1 + timedelta(minutes=time_1))
-        end_2 = (start_2 + timedelta(minutes=time_2))
-        duration_1 = end_1 - start_1
-        duration_2 = end_2 - start_2
-        total_duration = max(end_1, end_2) - min(start_1, start_2)
-        if total_duration < (duration_1 + duration_2):
-            raise forms.ValidationError(
-                {'start_time': ('Период пересекается'), },
-            )
+from schooling.form_validators import (
+    validate_intersections_time_periods,
+    validate_paid_lessons,
+    validate_teacher_subjects,
+)
 
 
 class LessonForm(forms.ModelForm):
@@ -46,42 +28,17 @@ class LessonForm(forms.ModelForm):
         datetime_start = self.cleaned_data['datetime_start']
         duration = self.cleaned_data['duration']
 
-        lessons_count = Lesson.objects.filter(
-            student_id=student,
-            test_lesson=False,
-            is_passed=False,
-        ).count()
-        if not test_lesson:
-            if lessons_count >= student.paid_lessons:
-                raise forms.ValidationError(
-                    {'student_id': _('Исчерпан лимит оплаченных занятий!')},
-                )
-        if subject not in teacher.competence.all():
-            raise forms.ValidationError(
-                {'subject': _('Предмет преподаёт другой преподаватель!')},
-            )
-
-        student_lessons = Lesson.objects.filter(
-            student_id=student,
-            datetime_start__date=date(
-                datetime_start.year,
-                datetime_start.month,
-                datetime_start.day,
-                ),
+        validate_paid_lessons(student=student, test_lesson=test_lesson)
+        validate_teacher_subjects(subject=subject, teacher=teacher,)
+        validate_intersections_time_periods(
+            user=student,
+            requested_time=datetime_start,
+            requested_lesson_duration=duration,
         )
-        validation_intersections_time_periods(
-            student_lessons, datetime_start, duration,
-        )
-        teacher_lessons = Lesson.objects.filter(
-            teacher_id=teacher,
-            datetime_start__date=date(
-                datetime_start.year,
-                datetime_start.month,
-                datetime_start.day,
-                ),
-        )
-        validation_intersections_time_periods(
-            teacher_lessons, datetime_start, duration,
+        validate_intersections_time_periods(
+            user=teacher,
+            requested_time=datetime_start,
+            requested_lesson_duration=duration,
         )
 
 
