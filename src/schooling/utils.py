ee23646ec6a10f26327ade2efe872f04cb4cc213
Konstantin Raikhert <raikhert13@gmail.com>
2024-08-06 18:11:18 +0300

    fix ruff warnings

--- src/schooling/utils.py
+++ src/schooling/utils.py
@@ -1,14 +1,11 @@
-from datetime import datetime
 
 
 def format_datetime(dt):
     """Функция для форматирования даты и времени в удобный формат."""
-
     return dt.strftime('%d %B, в %H.%M')
 
 
 def format_lesson_duration(start_time, end_time):
     """Функция для вычисления продолжительности занятия."""
-
     duration = (end_time - start_time).seconds // 60
     return f'{duration}'
