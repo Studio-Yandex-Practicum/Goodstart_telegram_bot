a877a078e3a1144926f66fcfd7fb2c6c8329817f
Konstantin Raikhert <raikhert13@gmail.com>
2024-09-10 11:38:29 +0300

    new settings

--- src/core/config/settings_base.py
+++ src/core/config/settings_base.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = ROOT_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
