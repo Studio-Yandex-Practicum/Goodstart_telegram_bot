7e2532446fdd2fa37f70ef2e9fbf725fc67d6647
Konstantin Raikhert <raikhert13@gmail.com>
2024-09-10 17:58:39 +0300

    fix urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, reverse_lazy
-from django.views.generic.base import RedirectView
+from django.urls import include, path
 
 urlpatterns = [
-    path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('admin/', admin.site.urls),
     path(
         'registration/',
