a5cf6ea902ee42ec25c3cf7ac2e311f8e9b2d1f2
IlyaPryzhykau <tuarone@gmail.com>
2025-02-11 02:41:25 +0100

    Обновил requirements/prod.txt через poetry export

--- src/core/apps.py
+++
@@ -1,7 +0,0 @@
-from material.admin.apps import AdminConfig
-
-
-class CustomAdminConfig(AdminConfig):
-    """Оверрайд для добавления касмомизированного класса AdminSite."""
-
-    default_site = 'core.admin.CustomAdminSite'
