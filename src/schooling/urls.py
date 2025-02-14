68b692c69da08509b17c02cad12dd6f3ef37b8e7
IlyaPryzhykau <tuarone@gmail.com>
2025-02-14 13:11:41 +0100

    bugfix/Remove_the_cancel/change_lesson_command

--- src/schooling/urls.py
+++ src/schooling/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from schooling.views import (
-    schedule_page, details_schedule_page, cancel_lesson,
-    change_datetime_lesson, lesson_change_success, lesson_cancel_success,
-)
+from schooling.views import (schedule_page, details_schedule_page)
+
 
 app_name = 'schedule'
 
@@ -13,25 +11,5 @@ urlpatterns = [
         '<int:id>/<int:lesson_id>/',
         details_schedule_page,
         name='details_schedule',
-    ),
-    path(
-        '<int:id>/<int:lesson_id>/change-datetime-lesson/',
-        change_datetime_lesson,
-        name='change_datetime_lesson',
-    ),
-    path(
-        '<int:id>/<int:lesson_id>/cancel-lesson/',
-        cancel_lesson,
-        name='cancel_lesson',
-    ),
-    path(
-        'lesson_change_success/',
-        lesson_change_success,
-        name='lesson_change_success',
-    ),
-    path(
-        'lesson_cancel_success/',
-        lesson_cancel_success,
-        name='lesson_cancel_success',
     ),
 ]
--- src/schooling/views.py
+++ src/schooling/views.py
@@ -1,18 +1,14 @@
-import pytz
 import datetime
 
-from django.urls import reverse, reverse_lazy
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render
 from asgiref.sync import sync_to_async
 from django.conf import settings
 
-from schooling.forms import ChangeDateTimeLesson
 from schooling.models import Teacher, Student, Lesson
 from schooling.signals_bot import (
     get_schedule_for_role, get_schedule_week_tasks,
 )
 from bot.utils import check_user_from_db
-from core.utils import send_change_lesson_email, send_cancel_lesson_email
 
 
 EMAIL_HOST_USER = settings.EMAIL_HOST_USER
@@ -81,55 +77,3 @@ async def details_schedule_page(request, id, lesson_id):
     return await sync_to_async(render)(
         request, 'schedule_details_card.html', context,
     )
-
-
-async def change_datetime_lesson(request, id, lesson_id):
-    form = ChangeDateTimeLesson()
-    if request.method == 'POST':
-        form = ChangeDateTimeLesson(request.POST)
-        if form.is_valid():
-            lesson = await Lesson.objects.aget(id=lesson_id)
-            new_datetime = form.cleaned_data['dt_field']
-            user = await check_user_from_db(id, (Student, Teacher))
-            moscow_tz = pytz.timezone('Europe/Moscow')
-            new_datetime_moscow = new_datetime.astimezone(moscow_tz)
-            formatted_datetime = new_datetime_moscow.strftime(
-                '%Y-%m-%d %H:%M:%S',
-            )
-            await send_change_lesson_email(lesson, user, formatted_datetime)
-            redirect_url = (
-                reverse('schedule:lesson_change_success') +
-                f'?new_datetime={formatted_datetime}'
-            )
-            return HttpResponseRedirect(redirect_url)
-    return await sync_to_async(render)(
-        request,
-        'schedule_change_dt_lesson.html',
-        context={'form': form},
-    )
-
-
-async def cancel_lesson(request, id, lesson_id):
-    if request.method == 'POST':
-        lesson = await Lesson.objects.aget(id=lesson_id)
-        user = await check_user_from_db(id, (Student, Teacher))
-        await send_cancel_lesson_email(lesson, user)
-        return await sync_to_async(HttpResponseRedirect)(
-            reverse_lazy('schedule:lesson_cancel_success'),
-        )
-    return await sync_to_async(render)(
-        request,
-        'schedule_cancel_lesson.html',
-    )
-
-
-def lesson_change_success(request):
-    new_datetime = request.GET.get('new_datetime')
-    return render(
-        request, 'lesson_change_success.html',
-        context={'new_datetime': new_datetime},
-    )
-
-
-def lesson_cancel_success(request):
-    return render(request, 'lesson_cancel_success.html')
