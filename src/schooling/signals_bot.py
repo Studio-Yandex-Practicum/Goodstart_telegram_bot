3d35c3aac09db6b792644b0380c4a64bbb4286fc
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2025-03-05 22:22:32 +0300

    change pre_delete on posr_delete

--- src/schooling/signals_bot.py
+++ src/schooling/signals_bot.py
@@ -9,7 +9,7 @@ from telegram._utils.types import ReplyMarkup
 from telegram.error import BadRequest, Forbidden
 from asgiref.sync import sync_to_async
 from django.conf import settings
-from django.db.models.signals import post_save, post_init, pre_delete, post_delete
+from django.db.models.signals import post_save, post_init, post_delete
 from django.dispatch import receiver
 from loguru import logger
 from pytz import timezone as pytz_timezone
