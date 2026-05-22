6f5ac4077ae1cb71cbdf45111735020003100c12
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2026-05-22 15:42:08 +0300

    test: proxy

--- src/bot/bot_interface.py
+++ src/bot/bot_interface.py
@@ -75,10 +75,12 @@ class Bot:
         # custom_request = HTTPXRequest(base_url=settings.TELEGRAM_BASE_URL)
 
         # Передаем custom_request в билдер через метод .request()
+
+        proxy_base_url = f"{settings.TELEGRAM_BASE_URL}bot" 
         app = (
             ApplicationBuilder()
             .token(settings.TELEGRAM_TOKEN)
-            .base_url(settings.TELEGRAM_BASE_URL)          # <-- Для обычных запросов
+            .base_url(proxy_base_url)          # <-- Для обычных запросов
             # .base_webhook_url(settings.TELEGRAM_BASE_URL)  # <-- Для вебхуков (на будущее)
             .persistence(persistence)
             .build()
